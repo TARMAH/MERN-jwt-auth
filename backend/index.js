81004aaddb58813d36bb83c8c4635856bb6cdbc2
Tarmah <tiqbal.bscs16seecs@seecs.edu.pk>
2021-05-04 22:02:45 +0500

    Set up BackEnd Server

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+// set up express
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
