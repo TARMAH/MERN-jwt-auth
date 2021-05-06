46be5ab4f70712584d311afab93a810a670216fe
Tarmah <tiqbal.bscs16seecs@seecs.edu.pk>
2021-05-07 02:08:15 +0500

    Set up MongoDb Connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 // set up express
 
@@ -11,3 +12,18 @@ app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
+
+// set up mongoose
+
+mongoose.connect( 
+    process.env.MONGODB_CONNECTION_STRING,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    },
+    (err) => {
+      if (err) throw err;
+      console.log("MongoDB connection established");
+    }
+  );
